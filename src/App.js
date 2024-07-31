13502e269ffef2d3261ea81fc9328db0c177b70a
Shriya Sorte <shriyasorte2000@gmail.com>
2024-07-31 16:50:24 +0530

    app js and css code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import UserList from "./components/UserList";
+import Navbar from "./components/Navbar";
+import UserDetails from "./components/UserDetails";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <UserList />
+      <Routes>
+        <Route path="/userDetails/:id" element={<UserDetails />} />
+      </Routes>
+    </>
   );
 }
 
